1b79a4f8b364bde7583a68a2d54cc88c36f1400f
pthung <pthung>
2018-10-01 17:06:15 +0700

    change name and package

--- android/app/src/main/java/com/windstudio/baymo/MainActivity.java
+++ android/app/src/main/java/com/windstudio/baymo/MainActivity.java
@@ -1,4 +1,4 @@
-package com.myshop;
+package com.windstudio.baymo;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "MyShop";
+        return "BayMo";
     }
 }
--- android/app/src/main/java/com/windstudio/baymo/MainApplication.java
+++ android/app/src/main/java/com/windstudio/baymo/MainApplication.java
@@ -1,4 +1,4 @@
-package com.myshop;
+package com.windstudio.baymo;
 
 import android.app.Application;
 
